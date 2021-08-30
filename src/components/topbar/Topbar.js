b48c2399d7f672b2141988ab952b92d224af192d
Shimanti <676501+Shimanti@users.noreply.github.com>
2021-08-29 21:20:52 -0700

    Beginning files

---
+++ src/components/topbar/Topbar.js
@@ -0,0 +1,11 @@
+import Home from './pages/home/Home'
+
+function Topbar() {
+  return (
+    <div className = "topbarContainer"
+    <div className="topberCenter"/>
+    <span className="logo".Lamasocial</span>
+    <div>clasName="searchba"
+  )
+
+export default App;
